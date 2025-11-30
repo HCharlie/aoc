e0c729148d992b3db8e189013c04ef421497ca90
Changli Han <changli.han@wolt.com>
2025-11-30 23:06:37 +0100

    clean up work

--- aoc-common/src/lib.rs
+++ aoc-common/src/lib.rs
@@ -2,13 +2,7 @@ mod types;
 mod aoc_client;
 mod runner;
 
-pub use types::*;
-pub use aoc_client::*;
-pub use runner::*;
-
-// Prelude for convenience
-pub mod prelude {
-    pub use crate::{Level, run_real, run_example};
-    pub use std::error::Error;
-}
-
+// Explicitly re-export only the public items needed from each module
+pub use types::Level;
+pub use aoc_client::{get_input_content, submit_check_answer};
+pub use runner::{run_example, run_real};
